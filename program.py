bde2ede8d2c2978e39fe0bfab9756791b02e04b5
bbecerikli <betulberrabe@gmail.com>
2025-10-22 15:23:39 +0300

    Liste yapilari ogrenildi.

--- program.py
+++ program.py
@@ -8,27 +8,30 @@ if __name__ =='__main__':
     print('-' * 100)
     print(f'| {baslik:^94} |x|')
     print('-' * 100)
-    
+     
+    ogrenci_adlari = []
+    ogrenci_soyadlari = []
+    ogrenci_nuamaralari = []
 
-    ogrenci1_adi = input('1.ogrencinin adini giriniz:')
-    ogrenci1_soyadi = input('1.ogrencinin soyadini giriniz:')
-    ogrenci1_numarasi = input('1.ogrencinin numaranizi giriniz:')
-    
-    ogrenci2_adi = input('2.ogrencinin adini giriniz:')
-    ogrenci2_soyadi = input('2.ogrencinin soyadini giriniz:')
-    ogrenci2_numarasi = input('2.ogrencinin numaranizi giriniz:')
+    ogrenci_adlari.append(input('1.ogrencinin adini giriniz:'))       #0.indeks
+    ogrenci_soyadlari.append(input('1.ogrencinin soyadini giriniz:')) #0.indeks
+    ogrenci_nuamaralari.append(input('1.ogrencinin numaranizi giriniz:')) #0.indeks
 
-    ogrenci3_adi = input('3.ogrencinin adini giriniz:')
-    ogrenci3_soyadi = input('3.ogrencinin soyadini giriniz:')
-    ogrenci3_numarasi = input('3.ogrencinin numaranizi giriniz:')
+    ogrenci_adlari.append(input('2.ogrencinin adini giriniz:'))      #1.indeks
+    ogrenci_soyadlari.append(input('2.ogrencinin soyadini giriniz:')) #1.indeks
+    ogrenci_nuamaralari.append(input('2.ogrencinin numaranizi giriniz:')) #1.indeks
+
+    ogrenci_adlari.append(input('3.ogrencinin adini giriniz:'))      #2.indeks
+    ogrenci_soyadlari.append(input('3.ogrencinin soyadini giriniz:')) #2.indeks
+    ogrenci_nuamaralari.append(input('3.ogrencinin numaranizi giriniz:')) #2.indeks
     
     print('-' * 100)
     
     print(f'|{' '*9}| {"Isim":<35} | {"Soyisim":<25} | {"Numara":<20} |')
     print('-' * 100)
-    print(f'| {1:>7d} | {ogrenci1_adi:<35} | {ogrenci1_soyadi:<25} | {ogrenci1_numarasi:<20} |')
-    print(f'| {2:>7d} | {ogrenci2_adi:<35} | {ogrenci2_soyadi:<25} | {ogrenci2_numarasi:<20} |')
-    print(f'| {3:>7d} | {ogrenci3_adi:<35} | {ogrenci3_soyadi:<25} | {ogrenci3_numarasi:<20} |')
+    print(f'| {1:>7d} | {ogrenci_adlari[0]:<35} | {ogrenci_soyadlari[0]:<25} | {ogrenci_nuamaralari[0]:<20} |')
+    print(f'| {2:>7d} | {ogrenci_adlari[1]:<35} | {ogrenci_soyadlari[1]:<25} | {ogrenci_nuamaralari[1]:<20} |')
+    print(f'| {3:>7d} | {ogrenci_adlari[2]:<35} | {ogrenci_soyadlari[2]:<25} | {ogrenci_nuamaralari[2]:<20} |')
 
 
 
@@ -36,8 +39,3 @@ if __name__ =='__main__':
 
 
 
-# print('|' , ' ' * 96, '|')
-# print('|' , ' ' * 96, '|')
-# print('|' , ' ' * 96, '|')
-# print('|' , ' ' * 96, '|')
-# print('-' * 100)
