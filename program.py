2f20a89a225468c09a5a0aedf806f710a1a5abea
bbecerikli <betulberrabe@gmail.com>
2025-10-22 14:33:37 +0300

    string format kullanmadan zorluklar.

--- program.py
+++ program.py
@@ -1,2 +1,34 @@
 if __name__ =='__main__':
-    print('Ogrenci bilgi sistemine hos geldiniz:')
+
+    baslik = 'Ogrenci Bilgi Sistemi (v1)'
+
+    print(baslik)
+    print(len(baslik))
+
+    print('-' * 100)
+    print('|',baslik, ' ' *(94 - (len(baslik)+1)), '|x|')
+    print('-' * 100)
+    
+
+    ogrenci_adi = input('ogrenci adini giriniz:')
+    ogrenci_soyadi = input('ogrenci soyadini giriniz:')
+    ogrenci_numarasi = input('ogrenci numaranizi giriniz:')
+    
+    
+    print('-' * 100)
+    print('|','Isim', ' ' *(94 - (len(baslik)+1)), '|x|')
+    print('-' * 100)
+
+
+
+
+
+
+
+
+
+# print('|' , ' ' * 96, '|')
+# print('|' , ' ' * 96, '|')
+# print('|' , ' ' * 96, '|')
+# print('|' , ' ' * 96, '|')
+# print('-' * 100)
