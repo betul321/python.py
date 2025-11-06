1c12ad1a17a58f715dedc485558e350afe8401de
bbecerikli <betulberrabe@gmail.com>
2025-11-06 11:51:28 +0300

    silme islemi icin alternatifler yapildi.

--- program.py
+++ program.py
@@ -12,9 +12,11 @@ if __name__ =='__main__':
           | listele | Ogrencileri listeler  |
           -----------------------------------
           ''')
-    ogrenci_adlari = []
-    ogrenci_soyadlari = []
-    ogrenci_nuamaralari = []
+    ogrenci_adlari = ['Betul','Turkan','Simge']
+    ogrenci_soyadlari = ['Becerikli','Topkara','Dogan']
+    ogrenci_numaralari = ['1234','1235','1236']
+    
+
     
     komut = input("Komut giriniz:").strip().lower()
     while komut != 'kapat':
@@ -25,27 +27,56 @@ if __name__ =='__main__':
             for sira in range(Ogrenci_sayisi):
                 ogrenci_adlari.append(input(f'{sira+1}.ogrencinin adini giriniz:'))      
                 ogrenci_soyadlari.append(input(f'{sira+1}.ogrencinin soyadini giriniz:')) 
-                ogrenci_nuamaralari.append(input(f'{sira+1}.ogrencinin numaranizi giriniz:')) 
+                ogrenci_numaralari.append(input(f'{sira+1}.ogrencinin numaranizi giriniz:')) 
             print('------------------------------------------------------')
         elif komut == 'sil':
             print('------------------------------------------------------')
-            print('Ogrenci silme komutlari!!!!')
+            Ogrenci_numarasi = (input('Ogrenci sayisini giriniz:'))
+            
+
+            #index = -1
+            #for i, numara in enumerate(ogrenci_numaralari):
+            #    if numara == Ogrenci_numarasi:
+            #        index = i
+        try:
+            index == ogrenci_numaralari.index(Ogrenci_numarasi)
+
+            ogrenci_numaralari.pop(index)
+            ogrenci_adlari.pop(index)
+            ogrenci_soyadlari.pop(index)
+            print(f'{Ogrenci_numarasi} numarali ogrenci silindi!')
+        except ValueError:
+            print(F'{Ogrenci_numarasi} numarali bir ogrenci yok!')
+            
+            #if index == -1:
+            #    print(F'{Ogrenci_numarasi} numarali bir ogrenci yok!'),
+            #else:
+            #    ogrenci_numaralari.pop(index)
+            #   ogrenci_adlari.pop(index)
+            #   ogrenci_soyadlari.pop(index)
+             #   print(f'{Ogrenci_numarasi} numarali ogrenci silindi!')
+                
+
             print('------------------------------------------------------')
         elif komut == 'listele':
             print('------------------------------------------------------')
+            Ogrenci_sayisi = int(input('Ogrenci sayisini giriniz:'))
             print('-' * 100)
             print(f'|{' '*2}| {"Isim":<12} | {"Soyisim":<8} | {"Numara":<6} |')
             print('-' * 100)
             for sira in range(len(Ogrenci_sayisi)):
-                print(f'| {(sira+1):>7d} | {ogrenci_adlari[sira]:<35} | {ogrenci_soyadlari[sira]:<25} | {ogrenci_nuamaralari[sira]:<20})
-                print('------------------------------------------------------')
+                print(f'| {(sira+1):>2} | {ogrenci_adlari[sira]:<12} | {ogrenci_soyadlari[sira]:<8} | {ogrenci_numaralari[sira]:<4}')
+                
+            print('------------------------------------------------------')
         else:
             print('------------------------------------------------------')
             print(f'"{komut} seklinde tanimli bir komut bulunmamaktadir.')
             print('------------------------------------------------------')
    
         komut = input('Komut giriniz:').strip().lower()
-        print('Program sonlandirildi.')
+        
+    print('------------------------------------------------------')
+    print('Program sonlandirildi.')
    
    
    
